13adc56cb0eeec31d2d227663947ed142fbe23f2
Andy Choi <wooseokchoi96@gmail.com>
2019-06-29 17:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function logShout(string){
   console.log(string.toUpperCase)
 }
 
+function logWhisper(string){
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string){
+  if(string)
+}
